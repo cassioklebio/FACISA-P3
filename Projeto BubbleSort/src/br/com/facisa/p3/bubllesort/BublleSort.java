45989c420f6cfa3db259eda87673e8ddeb9aa0dc
cassioklebio <cassiotr2009@gmail.com>
2017-10-16 13:02:19 -0300

    Bubble Sort

---
+++ Projeto BubbleSort/src/br/com/facisa/p3/bubllesort/BublleSort.java
@@ -0,0 +1,66 @@
+package br.com.facisa.p3.bubllesort;
+
+import java.util.Scanner;
+
+public class BublleSort {
+	
+	private int tamanho;
+	private int inseridos = 0;
+	private int[] ordenacao;
+	
+	
+	
+	public BublleSort(int tamanho) {
+		
+		ordenacao = new int[tamanho];
+	}
+
+
+
+	public void adicionar(int valor) {
+		ordenacao[inseridos] = valor;
+		inseridos++;		
+	}
+	
+	public void imprimiDesordenado() {
+		System.out.print("[");
+		for(int i = 0; i<ordenacao.length;i++) {
+			System.out.print(ordenacao[i]+" ");
+		}
+		System.out.print("]");
+	}
+	public void imprimiOrdenado() {
+		System.out.print("[");
+		for(int i = 0; i<ordenacao.length;i++) {
+			System.out.print(ordenacao[i]+" ");
+		}
+		System.out.print("]");
+	}
+	public void listaOrdenada() {
+		for(int i = 0; i < ordenacao.length; ++i){
+			 boolean controle = true;
+			 for(int j = 0; j < (ordenacao.length - 1); ++j){
+			 
+			 if(ordenacao[j] > ordenacao[j + 1]){
+			 int temp = ordenacao[j];
+			 ordenacao[j] = ordenacao[j + 1];
+			 ordenacao[j + 1] = temp;
+			 controle = false;
+			 }
+			 }
+			 if(controle){
+			 break;
+			 }
+			 
+			 }
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Projeto BubbleSort/src/br/com/facisa/p3/bubllesort/MainBuble.java
@@ -0,0 +1,38 @@
+package br.com.facisa.p3.bubllesort;
+
+import java.util.Scanner;
+
+public class MainBuble {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.print("Digite o tamanho do array  ");
+		int tamanho = ler.nextInt();
+		BublleSort buble = new BublleSort(tamanho);
+		for(int i = 0; i<tamanho;i++) {
+		System.out.print("Digite o numero ");
+		int valor = ler.nextInt();
+		buble.adicionar(valor);
+		}
+		buble.imprimiDesordenado();
+		
+		buble.listaOrdenada();
+		
+		buble.imprimiOrdenado();
+		
+		
+
+ 
+
+ 
+ 
+ 
+ 
+ 
+}
+
+		
+	}
+
+
